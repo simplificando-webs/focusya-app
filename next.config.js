9da4201d2b33795065de6bc3fe97d7613682c191
Patrick Toledo <simplificandowebs@gmail.com>
2025-04-22 19:36:38 +0200

    Remove Expo references from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-const { withExpo } = require('@expo/next-adapter');
-const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')(['react-native-web']);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ]
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      'react-native$': 'react-native-web',
-    };
-    return config;
-  },
 }
 
 module.exports = nextConfig; 
