e2b57f3c4067777212b57650f8f4f2a40ffc9503
Patrick Toledo <simplificandowebs@gmail.com>
2025-04-22 15:05:51 +0200

    Update Next.js config for static HTML

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
